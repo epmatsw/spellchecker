fd88e45312fe41b9dce5c76929cea7a4eb44330b
Will Stamper <epmatsw@gmail.com>
2014-08-23 13:15:29 -0500

    fixes to benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,10 @@
+#!/usr/local/bin/zsh
+
 git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
-time file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs node spellcheck > /dev/null
+file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs time node spellcheck
 git reset --hard HEAD
 git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
-time file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs node spellcheck > /dev/null
+file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs time node spellcheck
 git reset --hard HEAD
 git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
-time file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs node spellcheck > /dev/null
+file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs time node spellcheck
