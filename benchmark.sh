1571f7e5c6c1f8f8fd8ad0b337d9a07c3fc32502
Will Stamper <epmatsw@gmail.com>
2014-08-23 13:03:45 -0500

    updated benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,8 @@
 git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
-time find . -type f -not -path '*/\.*' -print0 | xargs -0 node spellcheck > /dev/null
+time file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs node spellcheck > /dev/null
 git reset --hard HEAD
 git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
-time find . -type f -not -path '*/\.*' -print0 | xargs -0 node spellcheck > /dev/null
+time file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs node spellcheck > /dev/null
 git reset --hard HEAD
 git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
-time find . -type f -not -path '*/\.*' -print0 | xargs -0 node spellcheck > /dev/null
+time file -I **/* | awk '/us-ascii/ {print substr($1, 0, length($1)-1)}' | xargs node spellcheck > /dev/null
