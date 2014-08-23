670831cb8b3b6fe11aba35eddf3e9e294c662e86
Will Stamper <epmatsw@gmail.com>
2014-08-23 13:02:41 -0500

    benchmarks

---
+++ benchmark.sh
@@ -0,0 +1,8 @@
+git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
+time find . -type f -not -path '*/\.*' -print0 | xargs -0 node spellcheck > /dev/null
+git reset --hard HEAD
+git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
+time find . -type f -not -path '*/\.*' -print0 | xargs -0 node spellcheck > /dev/null
+git reset --hard HEAD
+git checkout 168575f3f73f79185db0abdb91be364c0d0f29f9
+time find . -type f -not -path '*/\.*' -print0 | xargs -0 node spellcheck > /dev/null
